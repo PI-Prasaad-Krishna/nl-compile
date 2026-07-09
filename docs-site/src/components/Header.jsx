ba2428f06a8ec2bda07307be4e72ead1db3e82fc
PI-Prasaad-Krishna <prasaad2005@gmail.com>
2026-07-09 22:39:38 +0530

    Navigation flow update

--- docs-site/src/components/Header.jsx
+++ docs-site/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
   return (
     <header className="top-header">
       <Link to="/" className="header-logo">NL // Docs</Link>
       <div className="header-nav">
-        <Link to="/docs" className="header-link">Docs</Link>
+        {!isHome ? (
+          <Link to="/" className="header-link">← Home</Link>
+        ) : (
+          <Link to="/docs" className="header-link">Docs</Link>
+        )}
         <a href="https://github.com/PI-Prasaad-Krishna/nl-compile" target="_blank" rel="noreferrer" className="header-btn">
           GitHub
         </a>
